066344ecfee94426a76ce82762febb1946488b8c
Kazuki Maeda <kmaeda@kmaeda.net>
2016-01-22 10:20:25 +0900

    Add make_archive.sh.

---
+++ make_archive.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+PROJECT=ptex-texmf
+TMP=/tmp
+PREFIX=$PROJECT-`date '+%y%m%d%H%M%S'`
+
+git archive --format=tar --prefix=$PREFIX/ HEAD | (cd $TMP && tar xf -)
+echo " * Preprocess: iconv from UTF-8 to ISO-2022-JP"
+for f in `find $TMP/$PREFIX | sort`; do
+         if file $f | fgrep -q 'UTF-8' ; then
+             iconv -f UTF-8 -t ISO-2022-JP $f > $f.conv
+             mv $f.conv $f
+             echo "iconv: $f"
+         fi
+done
+
+echo
+echo " * Create $PREFIX.tar.gz"
+
+PWDF=`pwd`
+cd $TMP && tar zcfv $PWDF/$PREFIX.tar.gz $PREFIX/*
+rm -rf $TMP/$PREFIX
+echo
+echo " * Done: $PREFIX.tar.gz"
