e373d404ee4ea4f5a89890a90eed20d4315caf05
Kazuki Maeda <kmaeda@kmaeda.net>
2016-01-24 11:56:24 +0900

    Update make_archive.sh.

--- make_archive.sh
+++ make_archive.sh
@@ -6,16 +6,7 @@ PREFIX=$PROJECT-`date '+%y%m%d%H%M%S'`
 
 git archive --format=tar --prefix=$PREFIX/ HEAD | (cd $TMP && tar xf -)
 rm $TMP/$PREFIX/make_archive.sh
-echo " * Preprocess: iconv -f UTF-8 -t ISO-2022-JP"
-for f in `find $TMP/$PREFIX | sort`; do
-    if file $f | fgrep -q 'text' ; then
-        iconv -f UTF-8 -t ISO-2022-JP $f > $f.conv
-        mv $f.conv $f
-        echo "iconv: $f"
-    fi
-done
 
-echo
 echo " * Create $PREFIX.tar.gz"
 
 PWDF=`pwd`
